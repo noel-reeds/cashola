120ddbf1ffc1fb788b17ed37ba7c1b7e7a0af3b7
altoids <noelreeds01@gmail.com>
2026-07-24 10:36:11 +0300

    sends ping after 15 mins

--- keep-service-alive.sh
+++ keep-service-alive.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-while true; do
-  curl -s https://cashola.onrender.com/ > /dev/null
-  echo "$(date  '+%Y-%m-%dT%H:%M:%S') Ping sent!"
-  sleep 900
-done
+curl -s https://cashola.onrender.com/ > /dev/null
+echo "$(date  '+%Y-%m-%dT%H:%M:%S') Ping sent!"
